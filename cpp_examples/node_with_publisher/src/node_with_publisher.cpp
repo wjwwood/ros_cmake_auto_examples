ce8360b85d601dfe24dd9d759dea5c6b6bad8260
William Woodall <william@osrfoundation.org>
2016-12-22 17:17:51 -0800

    add node_with_publisher example

---
+++ cpp_examples/node_with_publisher/src/node_with_publisher.cpp
@@ -0,0 +1,42 @@
+// Copyright 2016 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <chrono>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+
+class NodeWithPublisher : public rclcpp::Node
+{
+  size_t count_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+public:
+  NodeWithPublisher() : rclcpp::Node("node_with_publisher"), count_(1) {
+    printf("In NodeWithPublisher()!\n");
+    publisher_ = this->create_publisher<std_msgs::msg::String>("chatter");
+    timer_ = this->create_wall_timer(1s, [this]() {
+      std_msgs::msg::String msg;
+      msg.data = "Hello World: " + std::to_string(count_++);
+      this->publisher_->publish(msg);
+      printf("Publishing: '%s'\n", msg.data.c_str());
+    });
+  }
+};
+
+#include "rclcpp_components/register_node_macro.hpp"
+
+RCLCPP_REGISTER_NODE(NodeWithPublisher)
